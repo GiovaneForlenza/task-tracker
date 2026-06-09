1919da6f2a08d697290f5539360ee649ca2eefdb
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2026-06-09 10:11:08 -0300

    refactor: remove SizeIndicator component and clean up imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import SizeIndicator from "./components/SizeIndicator";
-import { useState } from "react";
-import { useSelector } from "react-redux";
 
 function App() {
   const isModalOpen = useSelector((selector) => selector.modal.isOpen);
@@ -10,7 +8,6 @@ function App() {
     <main
       className={`flex ${isModalOpen ? "h-screen overflow-hidden" : "min-h-screen"} min-w-100 flex-col items-center justify-start bg-blue-50 p-5 px-2 md:px-5`}
     >
-      <SizeIndicator />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
